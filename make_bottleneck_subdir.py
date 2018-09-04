5257b546e074055e123920e93afe00aab0aeee41
z <kukharev.av@gmail.com>
2018-09-04 03:31:46 +0300

    1

--- make_bottleneck_subdir.py
+++ make_bottleneck_subdir.py
@@ -368,8 +368,8 @@ def make_bottleneck_dump_subdir(src_dir, shape):
 		data['labels']    = [x[1] for x in zip3]
 		data['filenames'] = [x[2] for x in zip3]
 
-	data = split_data.split_data(data, ratio=[1,1,1])
-
+	print('Split data')
+	data = split_data.split_data(data, ratio=[8,1,1])
 	data['id_label'] = map_id_label
 	data['label_id'] = map_label_id
 
