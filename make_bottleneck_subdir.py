f5691d72bac48f806e817deeccc69515405e2cc1
z <kukharev.av@gmail.com>
2018-09-04 03:11:29 +0300

    subdirs

--- make_bottleneck_subdir.py
+++ make_bottleneck_subdir.py
@@ -16,10 +16,13 @@ import numpy as np
 import tensorflow as tf
 import network
 
-DO_MIX = False
+import split_data
+
+DO_MIX = True
 
 if os.path.exists('.notebook'):
-	data_dir = '../data'
+	#data_dir = '../data'
+	data_dir = '../separated'	
 	module = network.conv_network_224
 else:
 	data_dir = '/home/chichivica/Data/Datasets/Scales/data'
@@ -221,7 +224,7 @@ def create_bootleneck_data(dir_path, shape, maps):
 
 
 
-def make_bottleneck_dump(in_dir, shape):
+def make_bottleneck_dump(src_dir, shape):
 
 	bottleneck_data = dict()
 	parts = ['valid', 'test', 'train']
@@ -257,12 +260,128 @@ def make_bottleneck_dump(in_dir, shape):
 	return bottleneck_data
 
 
-def save_data_dump(data, out_file):
+#------------------------------------------------
+
+def make_bottleneck_dump_subdir(src_dir, shape):
+	"""
+	Use names of subdirs as a id.
+	And then calculate class_index from id.
+	"""
+
+	class_id_set = set()
+	#bottleneck_data = dict()
+	feature_vectors, labels, filenames = [], [], []
+
+	image_size = (shape[0], shape[1])
+	listdir = os.listdir(src_dir)
+	
+	# 1) findout number of classes
+	for class_id in listdir:
+		
+		subdir = src_dir + '/' + class_id
+		if not os.path.isdir(subdir): continue
+
+		if len(os.listdir(subdir)) == 0: 
+			continue
+		else: 
+			try:
+				class_id_int = int(class_id)
+				class_id_set.add(class_id_int)
+			except:
+				continue	
+			
+	# 2) maps class_id to class_index			
+	id_list = list(class_id_set)
+	id_list.sort()
+	print('Number of classes in the sample: {0}'.format(len(id_list)))
+	print('Min class id: {0}'.format(min(id_list)))
+	print('Max class id: {0}'.format(max(id_list)))
+	map_id_label = {class_id : index for index, class_id in enumerate(id_list)}
+	map_label_id = {index : class_id for index, class_id in enumerate(id_list)}
+	maps = {'id_label' : map_id_label, 'label_id' : map_label_id}
+	num_classes = len(map_id_label)		
+
+	# 3) Calculate bottleneck in TF
+	height, width, color =  shape
+	x = tf.placeholder(tf.float32, [None, height, width, 3], name='Placeholder-x')
+	resized_input_tensor = tf.reshape(x, [-1, height, width, 3])
+	#module = hub.Module("https://tfhub.dev/google/imagenet/resnet_v2_152/classification/1")		
+	
+	# num_features = 2048, height x width = 224 x 224 pixels
+	assert height, width == hub.get_expected_image_size(module)	
+	bottleneck_tensor = module(resized_input_tensor)  # Features with shape [batch_size, num_features]
+	print('bottleneck_tensor:', bottleneck_tensor)			
+
+	with tf.Session() as sess:  # Connect to the TF runtime.
+		init = tf.global_variables_initializer()
+		sess.run(init)	# Randomly initialize weights.
+		
+		for class_id in class_id_set:
+
+			subdir = src_dir + '/' + str(class_id)
+			print(subdir)
+			files = os.listdir(subdir)
+			num_files = len(files)
+		
+			for index_file, filename in enumerate(files):
+
+				base = os.path.splitext(filename)[0]
+				ext = os.path.splitext(filename)[1]
+				if not ext in {'.jpg', ".png"} : continue			
+
+				class_index = map_id_label[class_id]
+				#print(class_index)
+
+				label = [0]*num_classes
+				label[class_index] = 1	
+
+				#class_index_set.add(class_index)	
+
+				file_path = subdir + '/' + filename
+				im = Image.open(file_path)		
+				im = im.resize(image_size, Image.ANTIALIAS)
+				arr = np.array(im, dtype=np.float32) / 256
+				feature_vector = bottleneck_tensor.eval(feed_dict={ x : [arr] })			
+
+				feature_vectors.append(feature_vector)
+				labels.append(label)
+				filenames.append(filename) # or file_path
+
+				im.close()
+
+				print("dir={0}, class={1}: {2}/{3}: {4}".format(class_id, class_index, index_file, num_files, filename))
+	
+	print('----')
+	print('Number of classes: {0}'.format(num_classes))	
+	print('Number of feature vectors: {0}'.format(len(feature_vectors)))	
+
+	data = {'images': feature_vectors, 'labels': labels, 'filenames':filenames}
+
+	# mix data	
+	if DO_MIX:
+		print('start mix data')
+		zip3 = list(zip(data['images'], data['labels'], data['filenames']))
+		random.shuffle(zip3)
+		print('mix ok')
+		data['images']    = [x[0] for x in zip3]
+		data['labels']    = [x[1] for x in zip3]
+		data['filenames'] = [x[2] for x in zip3]
+
+	bottleneck_data = split_data.split_data(data, ratio=[1,1,1])
+	bottleneck_data['id_label'] = map_id_label
+	bottleneck_data['label_id'] = map_label_id
+
+	return data
+
+
+#------------------------------------------------
+
+def save_data_dump(data, dst_file):
 	
 	# save the data on a disk
 	dump = pickle.dumps(bottleneck_data)
 	print('dump done')
-	f = gzip.open(out_file, 'wb')
+	f = gzip.open(dst_file, 'wb')
 	print('gzip done')
 	f.write(dump)
 	print('dump was written')
@@ -275,9 +394,9 @@ def createParser ():
 	ArgumentParser
 	"""
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-i', '--in_dir', default=None, type=str,\
+	parser.add_argument('-i', '--src_dir', default=None, type=str,\
 		help='input dir')
-	parser.add_argument('-o', '--out_file', default=None, type=str,\
+	parser.add_argument('-o', '--dst_file', default=None, type=str,\
 		help='output file')
 	parser.add_argument('-m', '--mix', dest='mix', action='store_true')
 
@@ -291,15 +410,18 @@ if __name__ == '__main__':
 	parser = createParser()
 	arguments = parser.parse_args(sys.argv[1:])			
 	NUM_ANGLES 	 = arguments.num
-	DO_MIX 		 = arguments.mix
+	#DO_MIX 		 = arguments.mix
 	print('NUM_ANGLES =', 	NUM_ANGLES)
-	print('DO_MIX =',		DO_MIX)
+	#print('DO_MIX =',		DO_MIX)
 
-	if not arguments.in_dir:
-		in_dir = data_dir
-	if not arguments.out_file:		
-		out_file = 'dump.gz'
+	if not arguments.src_dir:
+		src_dir = data_dir
+	if not arguments.dst_file:		
+		dst_file = 'dump.gz'
 
 	shape = 224, 224, 3	
-	bottleneck_data = make_bottleneck_dump(in_dir=in_dir, shape=shape)
-	save_data_dump(bottleneck_data, out_file=out_file)
+	#bottleneck_data = make_bottleneck_dump(in_dir=in_dir, shape=shape)
+
+	bottleneck_data = make_bottleneck_dump_subdir(src_dir=src_dir, shape=shape)	
+
+	save_data_dump(bottleneck_data, dst_file=dst_file)
