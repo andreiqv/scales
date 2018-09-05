1007370f06eff1f517f1aee215bbf00af048ba9f
z <kukharev.av@gmail.com>
2018-09-05 22:46:23 +0300

    class

--- make_bottleneck_subdir.py
+++ make_bottleneck_subdir.py
@@ -29,15 +29,18 @@ else:
 	#data_dir = '/home/chichivica/Data/Datasets/Scales/data'
 	data_dir = '/home/chichivica/Data/Datasets/Scales/separated'
 	import tensorflow_hub as hub
-	n_model = 2
-	if n_model == 1:
-		module = hub.Module("https://tfhub.dev/google/imagenet/resnet_v2_152/feature_vector/1")
+	model_number = 2
+	#type_model = 'feature_vector'
+	type_model = 'classification'
+	if model_number == 1:		
+		module = hub.Module("https://tfhub.dev/google/imagenet/resnet_v2_152/{0}/1".format(type_model))
 		SHAPE = 224, 224, 3
-	elif n_model == 2:
-		module = hub.Module("https://tfhub.dev/google/imagenet/inception_v3/feature_vector/1")
+	elif model_number == 2:
+		module = hub.Module("https://tfhub.dev/google/imagenet/inception_v3/{0}/1".format(type_model))
 		SHAPE = 299, 299, 3
 	else:
 		raise Exception('Bad n_model')
+		# https://tfhub.dev/google/imagenet/resnet_v2_152/feature_vector/1
 
 np.set_printoptions(precision=4, suppress=True)
 
